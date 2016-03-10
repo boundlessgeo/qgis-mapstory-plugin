9553582288e6ff2c69bee209c3d35082bf73cd44
Luigi Pirelli <luipir@gmail.com>
2016-03-10 18:00:56 +0100

    added GPL2 header and CONTRIBUTING

--- src/mapstory/tools/story.py
+++ src/mapstory/tools/story.py
@@ -1,3 +1,8 @@
+# -*- coding: utf-8 -*-
+#
+# (c) 2016 Boundless, http://boundlessgeo.com
+# This code is licensed under the GPL 2.0 license.
+#
 from mapstory.tools.utils import resourceFile
 import requests
 from mapstory.gui.executor import execute
